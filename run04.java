e78f7790efc44b23eb0bbd8c187e879ecd507568
Zamudo <92929359+Zamudo@users.noreply.github.com>
2021-11-01 10:29:38 -0400

    Add files via upload

---
+++ run04.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class run04 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class run04 extends Actor
+{
+    /**
+     * Act - do whatever the run04 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}

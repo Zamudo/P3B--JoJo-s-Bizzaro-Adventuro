bf0c98978162e9ebf948783b51e1b13fe23bb210
Joseph C <78003046+335534467@users.noreply.github.com>
2021-11-02 10:22:04 -0400

    Add files via upload
    added randomizer, 50 words to use in the game, stacks, arraylists

---
+++ jeff/StackOfWords.java
@@ -0,0 +1,69 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+/**
+ * Write a description of class WordsToArrayList here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StackOfWords extends Actor
+{
+    /**
+     * Act - do whatever the StackOfWords wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Stack StackOfWords()
+    {
+        Stack<String> stack = new Stack<String>();
+        stack.push("alligator");
+        stack.push("acrylic");
+        stack.push("acoustic");
+        stack.push("aftershave");
+        stack.push("alibi");
+        stack.push("anthropology");
+        stack.push("august");
+        stack.push("baby");
+        stack.push("bagel");
+        stack.push("balloon");
+        stack.push("banana");
+        stack.push("basketball");
+        stack.push("bathroom");
+        stack.push("beast");
+        stack.push("blizzard");
+        stack.push("bread");
+        stack.push("calculator");
+        stack.push("cat");
+        stack.push("cheese");
+        stack.push("chest");
+        stack.push("chimpanzee");
+        stack.push("coffee");
+        stack.push("cold");
+        //26th word
+        stack.push("cotton");
+        stack.push("crush");
+        stack.push("danger");
+        stack.push("death");
+        stack.push("dedication");
+        stack.push("desire");
+        stack.push("dirt");
+        stack.push("doctor");
+        stack.push("dream");
+        stack.push("duck");
+        stack.push("egg");
+        stack.push("exclamation");
+        stack.push("eyelash");
+        stack.push("family");
+        stack.push("feast");
+        stack.push("february");
+        stack.push("fertilizer");
+        stack.push("fiber");
+        stack.push("finger");
+        stack.push("fisherman");
+        stack.push("flame");
+        stack.push("flare");
+        stack.push("foam");
+        stack.push("frost");
+        stack.push("game");
+        
+        return stack;
+    }    
+}

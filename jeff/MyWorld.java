cc914e6bf0010dc239fd14c6f36ce349ee6ee3ce
Joseph C <78003046+335534467@users.noreply.github.com>
2021-11-03 10:17:04 -0400

    Add files via upload
    changed to phrases instead of random

--- jeff/MyWorld.java
+++ jeff/MyWorld.java
@@ -29,63 +29,6 @@ public class MyWorld extends World
         
         ArrayList<String> arr = new ArrayList<String>();
         
-        arr.add("alligator");
-        arr.add("acrylic");
-        arr.add("acoustic");
-        arr.add("aftershave");
-        arr.add("alibi");
-        arr.add("anthropology");
-        arr.add("august");
-        arr.add("baby");
-        arr.add("bagel");
-        arr.add("balloon");
-        arr.add("banana");
-        arr.add("basketball");
-        arr.add("bathroom");
-        arr.add("beast");
-        arr.add("blizzard");
-        arr.add("bread");
-        arr.add("calculator");
-        arr.add("cat");
-        arr.add("cheese");
-        arr.add("chest");
-        arr.add("chimpanzee");
-        arr.add("coffee");
-        arr.add("cold");
-        arr.add("cotton");
-        arr.add("crush");
-        arr.add("danger");
-        arr.add("death");
-        arr.add("dedication");
-        arr.add("desire");
-        arr.add("dirt");
-        arr.add("doctor");
-        arr.add("dream");
-        arr.add("duck");
-        arr.add("egg");
-        arr.add("exclamation");
-        arr.add("eyelash");
-        arr.add("family");
-        arr.add("feast");
-        arr.add("february");
-        arr.add("fertilizer");
-        arr.add("fiber");
-        arr.add("finger");
-        arr.add("fisherman");
-        arr.add("flame");
-        arr.add("flare");
-        arr.add("foam");
-        arr.add("frost");
-        arr.add("game");
-        arr.add("geometry");
-        arr.add("ghost");
-        
-        for(int i = 50; i > 0; i--)
-        {
-            int num = Greenfoot.getRandomNumber(i);
-            stack.push(arr.get(num));
-            System.out.println(arr.get(num));
-            arr.remove(num);
-        }
+        String input = Greenfoot.ask("frank");
     }
 }

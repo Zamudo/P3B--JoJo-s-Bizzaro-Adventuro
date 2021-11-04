ca772ee659b2b90b3e7746a44e9f91327bfd793d
bruhhh127 <93528194+bruhhh127@users.noreply.github.com>
2021-11-04 09:03:05 -0400

    Update MyWorld.java

--- jeff/MyWorld.java
+++ jeff/MyWorld.java
@@ -14,6 +14,7 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    String lvlOneString = "JoJo's mother, Ms. Holly, is a woman capable of calming the hearts of others. People feel at ease around her. This may sound awkward... but if I were to fall in love, I'd like it to be with someone like her. I would give my all for her. And I would want to always see her warm, happy smile.";
     public MyWorld()
     {    
         
@@ -30,5 +31,22 @@ public class MyWorld extends World
         ArrayList<String> arr = new ArrayList<String>();
         
         String input = Greenfoot.ask("frank");
+    }
+    public void levelOne()
+    {
+        countDown();
+        
+    }
+    public void countDown()
+    {
+        private int timer = 3000;
+        public void act()
+        {
+            timer--;
+            if (timer <= 0)
+            {
+                Greenfoot.stop();
+            }
+        }
     }
 }

e9226003a65fda6699c9059e4424a8d5c294bedf
Joseph C <78003046+335534467@users.noreply.github.com>
2021-11-05 10:47:28 -0400

    Add files via upload
    added random, stack, pushed array list into stack randomly, added comments

--- fronkie/MyWorld.java
+++ fronkie/MyWorld.java
@@ -2,6 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.*;
+import java.util.Random;
 /**
  * Write a description of class MyWorld here.
  * 
@@ -20,6 +22,7 @@ public class MyWorld extends greenfoot.World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
+        //Array list filled with words referencing the popular anime JoJo's Bizarre Adventure
         ArrayList<String> arr = new ArrayList<String>();
         arr.add("Stand Proud");
         arr.add("Death");
@@ -51,8 +54,23 @@ public class MyWorld extends greenfoot.World
         arr.add("Steel Ball");
         arr.add("Alternate Universe");
         arr.add("Justice");
+        
+        //String used to determine the word that the player will be guessing
         String currentWord;
         
+        //Stack to properly randomize the words that is used
+        Stack<String> stack = new Stack<String>();
+        
+        //Random to randomize the words
+        Random r = new Random();
+        
+        //Pushing the words from the array list to the stack in a random order
+        while(!arr.isEmpty())
+        {
+            int temp = r.nextInt(arr.size());
+            stack.push(arr.get(temp));
+            arr.remove(temp);
+        }
         if(Greenfoot.isKeyDown("1"))
         {
             currentWord = "turtle";
